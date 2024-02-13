f6342f2ba487039947336248935c55a83451a666
sniro <snirosh12@gmail.com>
2024-02-13 09:25:16 +0530

    update switch

--- src/main/java/org/example/Helpers/CanvasHelper.java
+++ src/main/java/org/example/Helpers/CanvasHelper.java
@@ -23,66 +23,11 @@ public class CanvasHelper {
             addNewElement();
         }
         switch (elementInput) {
-            case 1 -> {
-                System.out.print("Enter Length of a side of Square:");
-                double len = takeUserInput();
-                if (len > 0) {
-                    Square square = new Square(len);
-                    canvas.addElement(square);
-                } else {
-                    System.out.println("Element Not Added");
-                }
-            }
-            case 2 -> {
-                System.out.print("Enter Length of rectangle:");
-                double length = takeUserInput();
-                System.out.print("Enter Width of rectangle: ");
-                double width = takeUserInput();
-                if (length > 0 && width > 0) {
-                    Rectangle rectangle = new Rectangle(length, width);
-                    canvas.addElement(rectangle);
-                } else {
-                    System.out.println("Element Not Added");
-                }
-            }
-            case 3 -> {
-                System.out.print("Enter Radius of circle:");
-                double radius = takeUserInput();
-                if (radius > 0) {
-                    Circle circle = new Circle(radius);
-                    canvas.addElement(circle);
-                } else {
-                    System.out.println("Element Not Added");
-                }
-            }
-            case 4 -> {
-                System.out.print("Enter Length of side 1 of triangle: ");
-                double side1 = takeUserInput();
-                System.out.print("Enter Length of side 2 of triangle: ");
-                double side2 = takeUserInput();
-                System.out.print("Enter Length of side 3 of triangle: ");
-                double side3 = takeUserInput();
-                if (side1 > 0 && side2 > 0 && side3 > 0) {
-                    Triangle triangle = new Triangle(side1, side2, side3);
-                    canvas.addElement(triangle);
-                } else {
-                    System.out.println("Element Not Added");
-                }
-            }
-            case 5 -> {
-                System.out.print("Enter Length of top parallel line of trapezoid: ");
-                double length1 = takeUserInput();
-                System.out.print("Enter Length of bottom parallel line of trapezoid: ");
-                double length2 = takeUserInput();
-                System.out.print("Enter Height of trapezoid: ");
-                double height = takeUserInput();
-                if (length1 > 0 && length2 > 0 && height > 0) {
-                    Trapezoid trapezoid = new Trapezoid(length1,length2,height);
-                    canvas.addElement(trapezoid);
-                } else {
-                    System.out.println("Element Not Added");
-                }
-            }
+            case 1 -> addSquare();
+            case 2 -> addRectangle();
+            case 3 -> addCircle();
+            case 4 -> addTriangle();
+            case 5 -> addTrapezoid();
             case 9 -> {
             }
             default -> {
@@ -92,13 +37,69 @@ public class CanvasHelper {
         }
     }
 
+    private void addSquare() {
+        double len = takeUserInput("Enter Length of a side:");
+        if (len > 0) {
+            Square square = new Square(len);
+            canvas.addElement(square);
+        } else {
+            System.out.println("Element Not Added");
+        }
+    }
+
+    private void addCircle() {
+        double radius = takeUserInput("Enter Radius of circle:");
+        if (radius > 0) {
+            Circle circle = new Circle(radius);
+            canvas.addElement(circle);
+        } else {
+            System.out.println("Element Not Added");
+        }
+    }
+
+    private void addRectangle() {
+        double length = takeUserInput("Enter Length of rectangle:");
+        double width = takeUserInput("Enter width of rectangle: ");
+        if (length > 0 && width > 0) {
+            Rectangle rectangle = new Rectangle(length, width);
+            canvas.addElement(rectangle);
+        } else {
+            System.out.println("Element Not Added");
+        }
+    }
+
+    private void addTriangle() {
+        double side1 = takeUserInput("Enter Length of side 1 of triangle: ");
+        double side2 = takeUserInput("Enter Length of side 2 of triangle: ");
+        double side3 = takeUserInput("Enter Length of side 3 of triangle: ");
+        if (side1 > 0 && side2 > 0 && side3 > 0) {
+            Triangle triangle = new Triangle(side1, side2, side3);
+            canvas.addElement(triangle);
+        } else {
+            System.out.println("Element Not Added");
+        }
+    }
+
+    private void addTrapezoid() {
+        double length1 = takeUserInput("Enter Length of top parallel line of trapezoid: ");
+        double length2 = takeUserInput("Enter Length of bottom parallel line of trapezoid: ");
+        double height = takeUserInput("Enter Height of trapezoid: ");
+        if (length1 > 0 && length2 > 0 && height > 0) {
+            Trapezoid trapezoid = new Trapezoid(length1, length2, height);
+            canvas.addElement(trapezoid);
+        } else {
+            System.out.println("Element Not Added");
+        }
+    }
+
     public void calculateTotalArea() {
         System.out.println("Total Area of the Canvas is: " + canvas.totalArea());
     }
 
-    private double takeUserInput() {
+    private double takeUserInput(String message) {
         double value = 0;
         String valueString;
+        System.out.println(message);
         try {
             valueString = canvasScanner.nextLine();
             value = Double.parseDouble(valueString);
